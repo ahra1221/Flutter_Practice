82fc3dc67a86b15b485ef6e8304b49c0f84d9d04
ahra <ahravelop@gmail.com>
2025-01-13 16:36:56 +0900

    feat: child 구조 학습 및 적용

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,12 @@ void main() {
     home: Scaffold(
       body: Container(
         color: Colors.indigo,
+        padding: EdgeInsets.all(50),
+        child: Container(
+          width: 100,
+          height: 100,
+          color: Colors.redAccent,
+        ),
       ),
     )
   ));
