02d1cc45f54b066a03f517942fae1c4e1bf86591
ahra <ahravelop@gmail.com>
2025-01-14 14:32:35 +0900

    feat: Column 위젯을 활용한 버튼 3개 구현

--- lib/screen/button_screen.dart
+++ lib/screen/button_screen.dart
@@ -1,5 +1,4 @@
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ButtonScreen extends StatelessWidget {
@@ -8,6 +7,19 @@ class ButtonScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // Scaffold 화면에 기초가 되는 위젯
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            ElevatedButton(onPressed: (){}, child: Text("button1")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: (){}, child: Text("button2")),
+            SizedBox(height: 10),
+            ElevatedButton(onPressed: (){}, child: Text("button3"))
+          ],
+        ),
+      ),
+    );
   }
 }
