29d885ed92f23eda88a917b6a25e5725700e1f7d
ahra <ahravelop@gmail.com>
2025-01-16 15:10:01 +0900

    feat: 변수 타입 학습

---
+++ test/widget_test.dart
@@ -0,0 +1,26 @@
+
+void main() {
+  String name = "ahra";
+  print(name);
+
+  double weight = 10;
+  print(weight);
+
+  int age = 50;
+  print(age);
+
+  bool b = true;
+  print(b);
+
+  var name2 = "meme";
+
+  // dynamic 많이 사용하지 않음
+  dynamic name3 = "ahah";
+  name3 = 1;
+  name3 = false;
+
+  // 상수를 final, const
+  const name4 = "wpw";
+
+  int? ex = null;
+}
