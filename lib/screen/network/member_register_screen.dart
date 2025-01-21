d62fc710d7a339632c12c9039f9bbaff2d53b023
ahra <ahravelop@gmail.com>
2025-01-21 16:25:37 +0900

    feat: 회원조회 api 연결 및 기존 정보 바인딩 구현

--- lib/screen/network/member_register_screen.dart
+++ lib/screen/network/member_register_screen.dart
@@ -32,7 +32,7 @@ class _NetworkRegisterScreenState extends State<MemberRegisterScreen> {
             /// 회원가입 api 연결
 
             Dio dio = Dio(BaseOptions(
-                  baseUrl: "https://8bff-110-8-126-227.ngrok-free.app"
+                  baseUrl: "https://244b-110-8-126-227.ngrok-free.app"
                 )
             );
             
