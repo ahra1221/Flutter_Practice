f8229c225e3ac7d189284327164176d504f22380
ahra <ahravelop@gmail.com>
2025-01-21 14:20:39 +0900

    feat: 멤버목록조회 api 연결

--- lib/screen/network/network_screen.dart
+++ lib/screen/network/network_screen.dart
@@ -1,6 +1,7 @@
 
 import 'package:flutter/material.dart';
-import 'package:flutter_projects/screen/network/network_register_screen.dart';
+import 'package:flutter_projects/screen/network/member_list_screen.dart';
+import 'package:flutter_projects/screen/network/member_register_screen.dart';
 
 class NetworkScreen extends StatefulWidget {
   const NetworkScreen({super.key});
@@ -21,9 +22,18 @@ class _NetworkScreenState extends State<NetworkScreen> {
           ElevatedButton(
             onPressed: (){
               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return NetworkRegisterScreen();
+                return MemberRegisterScreen();
               }));
             }, child: Text("회원가입"),
+
+          ),
+          ElevatedButton(
+            onPressed: (){
+              Navigator.push(context, MaterialPageRoute(builder: (context) {
+                return MemberListScreen();
+              }));
+            }, child: Text("멤버 목록"),
+
           )
         ],
       ),
